bd7c064653eee5f5f52e6251fe9c44a7594bd148
Your Name <your.email@example.com>
2024-03-02 04:45:20 +0300

    2 trial

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,6 +1,2 @@
 #!/usr/bin/env ruby
-print ARGV[0].scan(/(?<=\[from:)[^\]]+/).join
-print ","
-print ARGV[0].scan(/(?<=\[to:)[^\]]+/).join
-print ","
-puts ARGV[0].scan(/(?<=\[flags:)[^\]]+/).join
+puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
