1fbbc19235e2b0bac3c6ceef02a1070a454e773b
Your Name <your.email@example.com>
2024-05-21 01:06:19 +0300

    jcheck

--- 0x15-api/all_json_check.py
+++ 0x15-api/all_json_check.py
@@ -1,35 +1,37 @@
 #!/usr/bin/python3
 """
-Check student JSON output
+Check student .CSV output of user information
 """
 
-import json
+import csv
 import requests
 import sys
 
-users_url = "https://jsonplaceholder.typicode.com/users"
+users_url = "https://jsonplaceholder.typicode.com/users?id="
 todos_url = "https://jsonplaceholder.typicode.com/todos"
 
 
-def user_info():
-    """ Check user info """
+def user_info(id):
+    """ Check CSV formatting """
 
-    with open('todo_all_employees.json', 'r') as f:
-        student_json = json.load(f)
-
-    correct_json = requests.get(users_url).json()
-
-    for correct_entry in correct_json:
+    response = requests.get(todos_url).json()
+    with open(str(id) + ".csv", 'r') as f:
+        output = f.read().strip()
+        count = 0
         flag = 0
-        for student_entry in student_json:
-            if str(correct_entry['id']) == student_entry:
-                flag = 1
-        if flag == 0:
-            print("User ID {} Found: Incorrect".format(correct_entry['id']))
-            return
+        for i in response:
+            if i['userId'] == id:
+                url = users_url + str(i['userId'])
+                usr_resp = requests.get(url).json()
+                line = '"' + str(i['userId']) + '","' + usr_resp[0]['username'] + '","' + str(i['completed']) + '","' + i['title'] + '"'
+                count += 1
+                if not line in output:
+                    print("Task {} Formatting: Incorrect".format(count))
+                    flag = 1
 
-    print("All users found: OK")
+    if flag == 0:
+        print("Formatting: OK")
 
 
 if __name__ == "__main__":
-    user_info()
+    user_info(int(sys.argv[1]))
