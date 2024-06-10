fe9ae9d4ace4a4d26442d02bd34f3cbfb4251b8a
brvke <tayebruke@gmail.com>
2024-06-10 13:47:52 +0000

    identationt

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 #fix bad "phpp" to "php" in "wp-settings.php"
 
 exec { 'fix-wordpress':
-  command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path => '/usr/local/bin/:/bin/',
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/',
 }
