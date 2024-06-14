57b75bf0eb57f1bbd3abc3abddd01e991daa548b
Your Name <your.email@example.com>
2024-06-14 17:50:08 +0300

    pam added

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,20 @@
-# adjusting user limits
-exec { 'change-os-configuration-for-holberton-user':
-  command => 'echo "holberton soft nofile 2048\nholberton hard nofile 4096" >> /etc/security/limits.conf', 
-  unless  => 'grep -q "holberton soft nofile 2048" /etc/security/limits.conf',
+#adjusting limit configuration and pam file
+exec { 'set-soft-limit':
+  command => 'echo "holberton soft nofile 4096" >> /etc/security/limits.conf',
+  unless  => '/bin/grep -q "holberton soft nofile 4096" /etc/security/limits.conf',
+}
+
+exec { 'set-hard-limit':
+  command => 'echo "holberton hard nofile 8192" >> /etc/security/limits.conf',
+  unless  => '/bin/grep -q "holberton hard nofile 8192" /etc/security/limits.conf',
+}
+
+exec { 'add-pam-limits-common-session':
+  command => 'echo "session required pam_limits.so" >> /etc/pam.d/common-session',
+  unless  => '/bin/grep -q "session required pam_limits.so" /etc/pam.d/common-session',
+}
+
+exec { 'add-pam-limits-common-session-noninteractive':
+  command => 'echo "session required pam_limits.so" >> /etc/pam.d/common-session-noninteractive',
+  unless  => '/bin/grep -q "session required pam_limits.so" /etc/pam.d/common-session-noninteractive',
 }
