f8b1dee36e4bc7be726ef58a970f78e2b75ece8d
Your Name <your.email@example.com>
2024-06-14 17:10:31 +0300

    documentaion

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,3 +1,4 @@
+# adjusting user limits
 exec { 'change-os-configuration-for-holberton-user':
   command => 'echo "holberton soft nofile 2048\nholberton hard nofile 4096" >> /etc/security/limits.conf', 
   unless  => 'grep -q "holberton soft nofile 2048" /etc/security/limits.conf',
