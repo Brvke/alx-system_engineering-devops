c937db6571377aadf4c586becf726fdafacee221
Your Name <your.email@example.com>
2024-06-14 18:07:43 +0300

    path edit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,20 +1,20 @@
-#adjusting limit configuration and pam file
+#adjust user limits and pam configuration
 exec { 'set-soft-limit':
-  command => 'echo "holberton soft nofile 4096" >> /etc/security/limits.conf',
-  unless  => '/bin/grep -q "holberton soft nofile 4096" /etc/security/limits.conf',
+  command => '/bin/echo "holberton soft nofile 2048" >> /etc/security/limits.conf',
+  unless  => '/bin/grep -q "holberton soft nofile 2048" /etc/security/limits.conf',
 }
 
 exec { 'set-hard-limit':
-  command => 'echo "holberton hard nofile 8192" >> /etc/security/limits.conf',
-  unless  => '/bin/grep -q "holberton hard nofile 8192" /etc/security/limits.conf',
+  command => '/bin/echo "holberton hard nofile 4096" >> /etc/security/limits.conf',
+  unless  => '/bin/grep -q "holberton hard nofile 4096" /etc/security/limits.conf',
 }
 
 exec { 'add-pam-limits-common-session':
-  command => 'echo "session required pam_limits.so" >> /etc/pam.d/common-session',
+  command => '/bin/echo "session required pam_limits.so" >> /etc/pam.d/common-session',
   unless  => '/bin/grep -q "session required pam_limits.so" /etc/pam.d/common-session',
 }
 
 exec { 'add-pam-limits-common-session-noninteractive':
-  command => 'echo "session required pam_limits.so" >> /etc/pam.d/common-session-noninteractive',
+  command => '/bin/echo "session required pam_limits.so" >> /etc/pam.d/common-session-noninteractive',
   unless  => '/bin/grep -q "session required pam_limits.so" /etc/pam.d/common-session-noninteractive',
 }
