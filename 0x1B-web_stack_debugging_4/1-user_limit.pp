a8df414ffd29c1727f05862f0c87e3241c321a93
Your Name <your.email@example.com>
2024-06-14 17:04:42 +0300

    trying to set limit fd

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,4 @@
+exec { 'change-os-configuration-for-holberton-user':
+  command => 'echo "holberton soft nofile 2048\nholberton hard nofile 4096" >> /etc/security/limits.conf', 
+  unless  => 'grep -q "holberton soft nofile 2048" /etc/security/limits.conf',
+}
